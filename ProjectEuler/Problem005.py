97910dffc8e11f21d6fbd4d1d323e32c2759f295
Matthew W. Noble <matthew.noble.1991@gmail.com>
2017-12-04 13:44:07 +0000

    Update Problem005.py

--- ProjectEuler/Problem005.py
+++ ProjectEuler/Problem005.py
@@ -2,16 +2,27 @@
 Project Euler - Problem 005: Smallest multiple
 """
 
-def gcd(a, b):
-    return b and gcd(b, a%b) or a
+import math
 
-def lcm(a, b):
-    return a*b//gcd(a, b)
+N = 20
+check_list = list(range(1 , N + 1))
 
-n=1
+# Question specific: Much faster if we start at N = 2520 and step in 2520's
+def find_solution(step):
+    for num in range(2520, math.factorial(N) + 1, 2520):
+        if all(num % n == 0 for n in check_list):
+            return num
+    return None
 
-for i in range(1, 21):
-    n = lcm(n, i)
+#def find_solution(step):
+#    for num in range(N, math.factorial(N) + 1, N):
+#        if all(num % n == 0 for n in check_list):
+#            return num
+#    return None
 
-print (n)
-
+if __name__ == '__main__':
+    solution = find_solution(N)
+    if solution is None:
+        print("No answer found")
+    else:
+        print("Found an answer:", solution)
