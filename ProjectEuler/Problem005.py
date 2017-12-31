7c09e4e3140538351d488578b3f6c48fd35773fb
Matthew W. Noble <matthew.noble.1991@gmail.com>
2017-12-31 23:02:42 +0000

    Add files via upload

--- ProjectEuler/Problem005.py
+++ ProjectEuler/Problem005.py
@@ -7,8 +7,8 @@ import math
 N = 20
 check_list = list(range(1 , N + 1))
 
-# Question specific: Much faster if we start at N = 2520 and step in 2520's
-def find_solution(step):
+# Question specific: Faster if we start at N = 2520 and step in 2520's
+def findSolution(step):
     for num in range(2520, math.factorial(N) + 1, 2520):
         if all(num % n == 0 for n in check_list):
             return num
@@ -20,9 +20,4 @@ def find_solution(step):
 #            return num
 #    return None
 
-if __name__ == '__main__':
-    solution = find_solution(N)
-    if solution is None:
-        print("No answer found")
-    else:
-        print("Found an answer:", solution)
+print(findSolution(N))
